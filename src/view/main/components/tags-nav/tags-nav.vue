26b74eb1f3ff47185467a84fac30eb3fe0a35aad
thomas <cy_laq_2011@126.com>
2018-08-14 15:49:19 +0800

    1.新版本替代

--- src/view/main/components/tags-nav/tags-nav.vue
+++ src/view/main/components/tags-nav/tags-nav.vue
@@ -3,7 +3,7 @@
     <div class="close-con">
       <Dropdown transfer @on-click="handleTagsOption" style="margin-top:7px;">
         <Button size="small" type="text">
-          <Icon :size="18" type="ios-close-outline"></Icon>
+          <Icon :size="18" type="ios-close-circle-outline" />
         </Button>
         <DropdownMenu slot="list">
           <DropdownItem name="close-all">关闭所有</DropdownItem>
@@ -12,10 +12,14 @@
       </Dropdown>
     </div>
     <div class="btn-con left-btn">
-      <Button icon="chevron-left" type="text" @click="handleScroll(240)"></Button>
+      <Button type="text" @click="handleScroll(240)">
+        <Icon :size="18" type="ios-arrow-back" />
+      </Button>
     </div>
     <div class="btn-con right-btn">
-      <Button icon="chevron-right" type="text" @click="handleScroll(-240)"></Button>
+      <Button type="text" @click="handleScroll(-240)">
+        <Icon :size="18" type="ios-arrow-forward" />
+      </Button>
     </div>
     <div class="scroll-outer" ref="scrollOuter" @DOMMouseScroll="handlescroll" @mousewheel="handlescroll">
       <div ref="scrollBody" class="scroll-body" :style="{left: tagBodyLeft + 'px'}">
@@ -29,7 +33,7 @@
             @on-close="handleClose"
             @click.native="handleClick(item)"
             :closable="item.name !== 'home'"
-            :color="item.name === value.name ? 'blue' : 'default'"
+            :color="item.name === value.name ? 'primary' : 'default'"
           >{{ showTitleInside(item) }}</Tag>
         </transition-group>
       </div>
@@ -52,7 +56,9 @@ export default {
   },
   data () {
     return {
-      tagBodyLeft: 0
+      tagBodyLeft: 0,
+      rightOffset: 40,
+      outerPadding: 4
     }
   },
   methods: {
@@ -65,14 +71,16 @@ export default {
       this.handleScroll(delta)
     },
     handleScroll (offset) {
+      const outerWidth = this.$refs.scrollOuter.offsetWidth
+      const bodyWidth = this.$refs.scrollBody.offsetWidth
       if (offset > 0) {
         this.tagBodyLeft = Math.min(0, this.tagBodyLeft + offset)
       } else {
-        if (this.$refs.scrollOuter.offsetWidth < this.$refs.scrollBody.offsetWidth) {
-          if (this.tagBodyLeft < -(this.$refs.scrollBody.offsetWidth - this.$refs.scrollOuter.offsetWidth)) {
+        if (outerWidth < bodyWidth) {
+          if (this.tagBodyLeft < -(bodyWidth - outerWidth)) {
             this.tagBodyLeft = this.tagBodyLeft
           } else {
-            this.tagBodyLeft = Math.max(this.tagBodyLeft + offset, this.$refs.scrollOuter.offsetWidth - this.$refs.scrollBody.offsetWidth)
+            this.tagBodyLeft = Math.max(this.tagBodyLeft + offset, outerWidth - bodyWidth)
           }
         } else {
           this.tagBodyLeft = 0
@@ -86,8 +94,15 @@ export default {
         this.$emit('on-close', res, 'all')
       } else {
         // 关闭除当前页和home页的其他页
-        let res = this.list.filter(item => item.name === this.value.name || item.name === 'home')
+        let currentName = ''
+        let res = this.list.filter(item => {
+          if (item.name === this.value.name) currentName = item.name
+          return item.name === this.value.name || item.name === 'home'
+        })
         this.$emit('on-close', res, 'others')
+        setTimeout(() => {
+          this.getTagElementByName(currentName)
+        }, 100)
       }
     },
     handleClose (e, name) {
@@ -99,7 +114,44 @@ export default {
     },
     showTitleInside (item) {
       return showTitle(item, this)
+    },
+    moveToView (tag) {
+      const outerWidth = this.$refs.scrollOuter.offsetWidth
+      const bodyWidth = this.$refs.scrollBody.offsetWidth
+      if (bodyWidth < outerWidth) {
+        this.tagBodyLeft = 0
+      } else if (tag.offsetLeft < -this.tagBodyLeft) {
+        // 标签在可视区域左侧
+        this.tagBodyLeft = -tag.offsetLeft + this.outerPadding
+      } else if (tag.offsetLeft > -this.tagBodyLeft && tag.offsetLeft + tag.offsetWidth < -this.tagBodyLeft + outerWidth) {
+        // 标签在可视区域
+        this.tagBodyLeft = Math.min(0, outerWidth - tag.offsetWidth - tag.offsetLeft - this.outerPadding)
+      } else {
+        // 标签在可视区域右侧
+        this.tagBodyLeft = -(tag.offsetLeft - (outerWidth - this.outerPadding - tag.offsetWidth))
+      }
+    },
+    getTagElementByName (name) {
+      this.$nextTick(() => {
+        this.refsTag = this.$refs.tagsPageOpened
+        this.refsTag.forEach((item, index) => {
+          if (name === item.name) {
+            let tag = this.refsTag[index].$el
+            this.moveToView(tag)
+          }
+        })
+      })
     }
+  },
+  watch: {
+    '$route' (to) {
+      this.getTagElementByName(to.name)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.getTagElementByName(this.$route.name)
+    }, 200)
   }
 }
 </script>
