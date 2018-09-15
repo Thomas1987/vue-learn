caa88a110dff6f376d36c4d8ef6bcf55f63fff4e
thomas <cy_laq_2011@126.com>
2018-09-15 11:41:57 +0800

    1.删除多余组建和通知组建

--- src/router/routers.js
+++ src/router/routers.js
@@ -44,31 +44,6 @@ export default [
     ]
   },
   {
-    path: '',
-    name: 'doc',
-    meta: {
-      title: '文档',
-      href: 'https://lison16.github.io/iview-admin-doc/#/',
-      icon: 'ios-book'
-    }
-  },
-  {
-    path: '/join',
-    name: 'join',
-    component: Main,
-    children: [
-      {
-        path: 'join_page',
-        name: 'join_page',
-        meta: {
-          icon: '_qq',
-          title: 'QQ群'
-        },
-        component: () => import('@/view/join-page.vue')
-      }
-    ]
-  },
-  {
     path: '/components',
     name: 'components',
     meta: {
